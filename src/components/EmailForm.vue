84beb729a19c543ad4357ce942ed57ee8757795d
vadim4web <vadim4web@gmail.com>
2025-04-10 22:19:33 +0300

    mail input fixex and improvements

--- src/components/EmailForm.vue
+++ src/components/EmailForm.vue
@@ -5,7 +5,7 @@
         <path d="M3 5V4C2.73478 4 2.48043 4.10536 2.29289 4.29289C2.10536 4.48043 2 4.73478 2 5H3ZM21 5H22C22 4.73478 21.8946 4.48043 21.7071 4.29289C21.5196 4.10536 21.2652 4 21 4V5ZM3 6H21V4H3V6ZM20 5V17H22V5H20ZM19 18H5V20H19V18ZM4 17V5H2V17H4ZM5 18C4.73478 18 4.48043 17.8946 4.29289 17.7071C4.10536 17.5196 4 17.2652 4 17H2C2 17.7956 2.31607 18.5587 2.87868 19.1213C3.44129 19.6839 4.20435 20 5 20V18ZM20 17C20 17.2652 19.8946 17.5196 19.7071 17.7071C19.5196 17.8946 19.2652 18 19 18V20C19.7956 20 20.5587 19.6839 21.1213 19.1213C21.6839 18.5587 22 17.7956 22 17H20Z" fill="#64748B" />
         <path d="M3 5L12 14L21 5" stroke="#64748B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
-      <input id="email" v-model="email" type="email" required :placeholder="$t('email_placeholder')">
+      <input id="email" v-model="email" type="email" required :placeholder="$t('email_placeholder')" autocomplete="mail">
     </label>
     <label for="submit" class="submit">
       <button id="submit" type="submit">
@@ -104,6 +104,19 @@ const submitForm = async () => {
     &:focus,
     &:focus-visible {
       outline: none;
+    }
+
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill:active {
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: black;
+      box-shadow: inset 0 0 3rem 3rem white;
+
+      &::selection {
+        -webkit-text-fill-color: white;
+      }
     }
   }
 
