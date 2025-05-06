f0423ef96ea1012c87b7b61acc6f76a4e5e1e69c
vadim4web <vadim4web@gmail.com>
2025-05-06 17:51:54 +0300

    save before splitting code

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,23 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig(({ command, mode }) => {
-	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-
-	const isProduction = mode === 'production' || command === 'build'
-	// const base = isProduction ? '/gouri/' : '/' // ! should be set to '/'
-	const base = '/' // ! should be set to '/'
-
-	return {
-    plugins: [
-      vue(),
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-        '~': path.resolve(__dirname, 'src'),
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
+  base,
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
       },
     },
-    base,
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler',
-        },
-      },
-    },
-	}
+  },
 })
