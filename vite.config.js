b78d8ef56f9c5345963149a020334d837d58a066
vadim4web <vadim4web@gmail.com>
2026-03-05 09:42:48 +0200

    last version with CRITICAL CSS

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,54 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-// import critical from 'rollup-plugin-critical'
+import critical from 'rollup-plugin-critical'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    // ⚙️ Генерація критичного CSS після білду
-    // critical({
-    //   criticalUrl: 'https://gouri.com.pl/',
-    //   criticalBase: 'dist/',
-    //   criticalPages: [
-    //     { uri: '', template: 'index' },
-    //   ],
-    //   criticalConfig: {
-    //     inline: true,
-    //     extract: false,
-    //     width: 375,
-    //     height: 812,
-    //     penthouse: {
-    //       blockJSRequests: false,
-    //     },
-    //   },
-    // }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '~': path.resolve(__dirname, 'src'),
-    },
-  },
-  base: '/',
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name][extname]',
-      },
-    },
-  },
+	plugins: [
+		vue(),
+		// ⚙️ Генерація критичного CSS після білду
+		critical({
+			criticalUrl: 'https://gouri.com.pl/',
+			criticalBase: 'dist/',
+			criticalPages: [
+				{ uri: '', template: 'index' },
+			],
+			criticalConfig: {
+				inline: true,
+				extract: false,
+				width: 375,
+				height: 812,
+				penthouse: {
+					blockJSRequests: false,
+				},
+			},
+		}),
+	],
+
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'~': path.resolve(__dirname, 'src'),
+		},
+	},
+
+	base: '/',
+
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler',
+			},
+		},
+	},
+
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: 'assets/[name].js',
+				chunkFileNames: 'assets/[name].js',
+				assetFileNames: 'assets/[name][extname]',
+			},
+		},
+	},
 })
